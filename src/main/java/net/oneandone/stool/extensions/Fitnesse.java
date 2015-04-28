e17eb3a1a5dad7678dce6d7ecc97edf367f2bec0
Michael Hartmeier <mlhartme@github.com>
2015-04-28 13:37:06 +0200

    Fitnesse from Goekhan

--- src/main/java/net/oneandone/stool/extensions/Fitnesse.java
+++ src/main/java/net/oneandone/stool/extensions/Fitnesse.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Fitnesse implements Extension {
     private static final String FITNESSSE = "fitnesse";
@@ -28,11 +30,19 @@ public class Fitnesse implements Extension {
         Console console;
         Ports ports;
         int port;
-
         console = stage.session.console;
         ports = stage.loadPortsOpt();
         port = ports.lookup(FITNESSSE).httpPort();
-        console.info.println("start port " + port);
+        String projectDir = findProjectDir(ports);
+
+        stage.launcher("sh", projectDir + "/src/test/resources/fitnesse-start.sh", String.valueOf(port), projectDir).exec(console.info,console.error);
+        String url = findUrl(ports);
+        console.info.println("fitnesse start: " + url.concat(":" + port));
+    }
+
+    private String findProjectDir(Ports ports) {
+        String path = ports.mainHost().docBase();
+        return path.substring(0, path.indexOf("/target"));
     }
 
     @Override
@@ -44,7 +54,35 @@ public class Fitnesse implements Extension {
         console = stage.session.console;
         ports = stage.loadPortsOpt();
         port = ports.lookup(FITNESSSE).httpPort();
-        console.info.println("stop port " + port);
+        String url = findUrl(ports);
+        String projectDir = findProjectDir(ports);
+        projectDir = hackForProjectDir(projectDir);
+
+        stage.launcher("sh",projectDir + "/src/test/resources/fitnesse-stop.sh", url, String.valueOf(port), projectDir).exec(console.info,console.error);
+        console.info.println("fitnesse stop: " + url.concat(":" + port));
+    }
+
+
+    private String hackForProjectDir(String projectDir){
+        try{
+            String[] split = projectDir.split("/");
+            //project dir includes duplicates
+            if(split[split.length - 1].equals(split[split.length -2])) {
+                return projectDir.substring(0, projectDir.lastIndexOf(("/" + split[split.length - 2])));
+            }
+        }catch (Exception e) {
+            return projectDir;
+        }
+        return projectDir;
+    }
+
+    private String findUrl(Ports ports) {
+        String url = ports.mainHost().httpUrl(false);
+        Pattern pattern = Pattern.compile("(:[0-9]+)");
+        Matcher matcher = pattern.matcher(url);
+        matcher.find();
+        String port = matcher.group();
+        return url.substring(0, url.indexOf(port)); //exclude port
     }
 
     @Override
