877406d15d1869818a59a75909266238a5b048a4
Michael Hartmeier <mlhartme@github.com>
2015-04-28 13:27:42 +0200

    docs

--- src/main/java/net/oneandone/stool/extensions/Extension.java
+++ src/main/java/net/oneandone/stool/extensions/Extension.java
@@ -11,7 +11,11 @@ import java.util.Map;
 public interface Extension {
     Map<String, FileNode> vhosts(Stage stage);
 
+    /**
+     *  @param apps urls of all applications (http and - if cert generation is configured - https)
+     */
     void beforeStart(Stage stage, Collection<String> apps) throws IOException;
+
     void beforeStop(Stage stage) throws IOException;
 
     void contextParameter(Stage stage, String host, int httpPort, FileNode webinf, Map<String, String> result) throws XmlException;
