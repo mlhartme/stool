08b4ed8e8715f73125b3606623e1bdf1a3af8733
Michael Hartmeier <mlhartme@github.com>
2015-05-12 01:07:50 +0200

    quoting fix

--- src/main/resources/templates/stool/bin/stool-raw.sh
+++ src/main/resources/templates/stool/bin/stool-raw.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # Stool overview invokes this script to execute stool commands with the login environment of the stage owner.
 # This script needs sudo permissions.
-bash --login -c "source ${{stool.home}}/bin/stool-function && stool $@"
+bash --login -c "source ${{stool.home}}/bin/stool-function && stool $*"
