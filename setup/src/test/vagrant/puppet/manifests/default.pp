436b479224476f2b7298db39f208f42a0b284f2e
Michael Hartmeier <mlhartme@github.com>
2015-11-06 13:05:03 +0100

    our repository to test apt-get

--- setup/src/test/vagrant/puppet/manifests/default.pp
+++ setup/src/test/vagrant/puppet/manifests/default.pp
@@ -4,6 +4,17 @@ apt::source { 'backports':
   repos   => 'main',
 }
 ->
+apt::source { 'ciso':
+  location => 'http://reposrv.loc.lan/1and1/ciso/contrib',
+  architecture => 'all',
+  release => 'jessie',
+  repos   => 'contrib',
+  key => {
+    id     => '1F2860A861A8B825825035753046B215008E6F95',
+    source => 'http://reposrv.loc.lan/1and1/ciso/key.asc'
+  },
+}
+->
 Class['apt::update']
 ->
 package { 'openjdk-8-jdk' : }
