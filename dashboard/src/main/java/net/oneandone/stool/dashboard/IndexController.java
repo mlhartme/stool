349e174a8b2c2ac388435709a3f49734805dbe89
Maximilian Braun <me+github@max-braun.com>
2016-02-22 08:51:32 +0100

    Update IndexController.java

--- dashboard/src/main/java/net/oneandone/stool/dashboard/IndexController.java
+++ dashboard/src/main/java/net/oneandone/stool/dashboard/IndexController.java
@@ -52,10 +52,13 @@ public class IndexController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(ModelAndView modelAndView) {
         Object username;
-
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
         username = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (username instanceof InetOrgPerson) {
-            username = ((InetOrgPerson) username).getDisplayName();
+            if (username instanceof InetOrgPerson) {
+                username = ((InetOrgPerson) username).getDisplayName();
+            }
+        } else {
+            username = "Not logged in"
         }
         modelAndView.setViewName("index");
         modelAndView.addObject("username", username);
