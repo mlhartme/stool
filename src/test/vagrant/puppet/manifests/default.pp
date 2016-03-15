124d6a80480cae63fd9dfe9dfecf85ba4e929aea
Michael Hartmeier <mlhartme@github.com>
2016-03-15 13:48:08 +0100

    vagrant update

--- src/test/vagrant/puppet/manifests/default.pp
+++ src/test/vagrant/puppet/manifests/default.pp
@@ -4,9 +4,21 @@ apt::source { 'backports':
   release => "jessie-backports",
   repos   => 'main',
 }
-->
-apt::source { 'ciso':
-  location => 'http://reposrv.loc.lan/1and1/ciso/contrib',
+# for ciso tools
+apt::source { 'ciso-stable':
+  location => 'http://reposrv.loc.lan/1and1/ciso/stable',
+  architecture => 'all',
+  release => 'jessie',
+  repos   => 'contrib',
+  key => {
+    id     => '1F2860A861A8B825825035753046B215008E6F95',
+    source => 'http://reposrv.loc.lan/1and1/ciso/key.asc'
+  },
+}
+
+# for unstable ciso tools
+apt::source { 'ciso-unstable':
+  location => 'http://reposrv.loc.lan/1and1/ciso/unstable',
   architecture => 'all',
   release => 'jessie',
   repos   => 'contrib',
