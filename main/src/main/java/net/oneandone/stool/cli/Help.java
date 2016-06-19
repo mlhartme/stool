2ce339d65c9f897f0996cb97590d41479acddb88
Michael Hartmeier <mlhartme@github.com>
2016-06-19 21:13:07 +0200

    fix internal man pages

--- main/src/main/java/net/oneandone/stool/cli/Help.java
+++ main/src/main/java/net/oneandone/stool/cli/Help.java
@@ -15,28 +15,61 @@
  */
 package net.oneandone.stool.cli;
 
+import net.oneandone.stool.util.Files;
 import net.oneandone.stool.util.Session;
+import net.oneandone.sushi.fs.World;
 import net.oneandone.sushi.fs.file.FileNode;
 
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
+
 public class Help {
-    private final FileNode manpath;
+    private final World world;
     private final String command;
 
     public Help(Session session, String command) {
-        this.manpath = session.lib.join("man");
+        this.world = session.world;
         this.command = command;
     }
 
     public void run() throws Exception {
         ProcessBuilder builder;
         Process process;
+        FileNode tmp;
 
         builder = new ProcessBuilder();
-        builder.environment().put("MANPATH", manpath.getAbsolute());
         builder.directory(null /* use current directory */);
-        builder.command("man", command == null ? "stool" : "stool-" + command);
-        builder.inheritIO();
+        builder.command("less");
+        tmp = format();
+        builder.redirectInput(tmp.toPath().toFile());
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
         process = builder.start();
         process.waitFor();
+    }
+
+    private FileNode format() throws Exception {
+        ProcessBuilder builder;
+        Process process;
+        String name;
+        FileNode tmp;
+
+        builder = new ProcessBuilder();
+        builder.directory(null /* use current directory */);
+        builder.command("groff", "-man", "-Tascii");
+        tmp = world.getTemp().createTempFile();
+        builder.redirectInput(ProcessBuilder.Redirect.PIPE);
+        builder.redirectOutput(tmp.toPath().toFile());
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+        process = builder.start();
+        name = command == null ? "stool" : "stool-" + command;
+        try (InputStream src = world.resource("templates/man/man1/" + name + ".1.gz").newInputStream()) {
+            try (GZIPInputStream unzip = new GZIPInputStream(src)) {
+                world.getBuffer().copy(unzip, process.getOutputStream());
+            }
+        }
+        process.getOutputStream().close();
+        process.waitFor();
+        return tmp;
     }
 }
