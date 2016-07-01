c5d077b56f8f9c815c3c4e442f509679afce6921
Michael Hartmeier <mlhartme@github.com>
2016-07-01 12:48:09 +0200

    multi string

--- main/src/main/java/net/oneandone/stool/util/Url.java
+++ main/src/main/java/net/oneandone/stool/util/Url.java
@@ -2,10 +2,10 @@ package net.oneandone.stool.util;
 
 import net.oneandone.inline.ArgumentException;
 import net.oneandone.sushi.util.Separator;
-import net.oneandone.sushi.util.Strings;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Url {
     private static final String CONTEXT = "!";
@@ -13,83 +13,158 @@ public class Url {
     public static Url parse(String url) {
         int beforeHost;
         int afterHost;
-        List<String> protocols;
-        List<String> hostnames;
-        List<String> paths;
-        int beforePaths;
+        String protocol;
+        String hostname;
+        String path;
+        int beforePath;
         String context;
 
         beforeHost = url.indexOf("://");
         if (beforeHost == -1) {
             throw new ArgumentException(url);
         }
-        protocols = list(url.substring(0, beforeHost));
+        protocol = url.substring(0, beforeHost);
         afterHost = url.indexOf('/', beforeHost + 3);
         if (afterHost == -1) {
             afterHost = url.length();
-            paths = new ArrayList<>();
+            path = "";
             context = null;
         } else {
-            beforePaths = url.indexOf(CONTEXT, afterHost + 1);
-            if (beforePaths == -1) {
-                beforePaths = afterHost + 1;
+            beforePath = url.indexOf(CONTEXT, afterHost + 1);
+            if (beforePath == -1) {
+                beforePath = afterHost + 1;
                 context = null;
             } else {
-                context = url.substring(afterHost + 1, beforePaths);
-                beforePaths++;
+                context = url.substring(afterHost + 1, beforePath);
+                beforePath++;
             }
-            paths = list(url.substring(beforePaths));
-
+            path = url.substring(beforePath);
         }
-        hostnames = list(url.substring(beforeHost + 3, afterHost));
-
-        return new Url(protocols, hostnames, context, paths);
+        hostname = url.substring(beforeHost + 3, afterHost);
+        return new Url(protocol, hostname, context, path);
     }
 
     private static final Separator SEP = Separator.on('|').trim();
 
-    private static List<String> list(String str) {
-        List<String> result;
-        if (str.startsWith("(")) {
-            str = Strings.removeRight(str.substring(1), ")");
-            result = SEP.split(str);
-        } else {
-            result = new ArrayList<>();
-            result.add(str);
+    //--
+
+    private final String protocol;
+    private final String hostname;
+    /** may be null */
+    private final String context;
+    private final String path;
+
+    public Url(String protocol, String hostname, String context, String path) {
+        this.protocol = protocol;
+        this.hostname = hostname;
+        this.context = context;
+        this.path = path;
+    }
+
+    public Url sustitute(Map<Character, String> map) {
+        return new Url(subst(protocol, map), subst(hostname, map), context == null ? null : subst(context, map), subst(path, map));
+    }
+
+    private static String subst(String str, Map<Character, String> map) {
+        int prev;
+        int pos;
+        StringBuilder builder;
+        String s;
+        char c;
+
+        builder = new StringBuilder();
+        prev = 0;
+        while (true) {
+            pos = str.indexOf('%', prev);
+            if (pos == -1 || pos == str.length() - 1) {
+                builder.append(str.substring(prev));
+                return builder.toString();
+            }
+            builder.append(str.substring(prev, pos));
+            c = str.charAt(pos + 1);
+            s = map.get(c);
+            if (s == null) {
+                throw new IllegalArgumentException("unknown variable: %" + c);
+            }
+            builder.append(s);
+            prev = pos + 2;
         }
-        return result;
+    }
+
+    public String toString() {
+        return protocol + "://" + hostname + "/" + (context == null ? "" : context + CONTEXT) + path;
+    }
+
+    public List<String> map() {
+        MultiString multiString;
+
+        multiString = new MultiString();
+        multiString.append(protocol);
+        multiString.append("://");
+        multiString.append(hostname);
+        multiString.append("/");
+        if (context != null) {
+            multiString.append(context + CONTEXT);
+        }
+        multiString.append(path);
+        return multiString.lst;
+    }
+
+    public boolean ssl() {
+        MultiString multiString;
+
+        multiString = new MultiString();
+        multiString.append(protocol);
+        return multiString.contains("https");
     }
 
     //--
 
-    private final List<String> protocols;
-    private final List<String> hostnames;
-    private final String context;
-    private final List<String> paths;
+    public static class MultiString {
+        public final List<String> lst;
 
-    public Url(List<String> protocols, List<String> hostnames, String context, List<String> paths) {
-        this.protocols = protocols;
-        this.hostnames = hostnames;
-        this.context = context;
-        this.paths = paths;
-    }
-
-    public String toString() {
-        return str(protocols) + "://" + str(hostnames) + "/" + (context == null ? "" : context + CONTEXT) + str(paths);
-    }
-
-    private static String str(List<String> all) {
-        switch (all.size()) {
-            case 0:
-                return "";
-            case 1:
-                return all.get(0);
-            default:
-                return '(' + SEP.join(all) + ')';
+        public MultiString() {
+            lst = new ArrayList<>();
+            lst.add("");
         }
-    }
 
-    public boolean ssl() {
-        return protocols.contains("https");
+        public void append(String str) {
+            int prev;
+            int open;
+            int close;
+            List<String> tmp;
+
+            prev = 0;
+            while (true) {
+                open = str.indexOf('(', prev);
+                if (open == -1) {
+                    appendAll(str.substring(prev, str.length()));
+                    return;
+                }
+                open++;
+                close = str.indexOf(')', open);
+                if (close == -1) {
+                    throw new IllegalArgumentException("closing ) not found: " + str);
+                }
+                tmp = new ArrayList<>(lst);
+                lst.clear();
+                for (String p : SEP.split(str.substring(open, close))) {
+                    for (String a : tmp) {
+                        lst.add(a + p);
+                    }
+                }
+                prev = close + 1;
+            }
+        }
+
+        private void appendAll(String str) {
+            for (int i = 0; i < lst.size(); i++) {
+                lst.set(i, lst.get(i) + str);
+            }
+        }
+
+        public boolean contains(String str) {
+            return lst.contains(str);
+        }
     }
 }
--- main/src/test/java/net/oneandone/stool/util/UrlTest.java
+++ main/src/test/java/net/oneandone/stool/util/UrlTest.java
@@ -2,44 +2,75 @@ package net.oneandone.stool.util;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 public class UrlTest {
     @Test
     public void normal() {
-        check("http://foo.bar/");
-        check("https://baz", "https://baz/");
-        check("https://baz/path");
-        check("https://baz/path/");
+        parse("http://foo.bar/");
+        parse("https://baz", "https://baz/");
+        parse("https://baz/path");
+        parse("https://baz/path/");
     }
 
     @Test
     public void protocols() {
-        check("(http|https)://foo.bar/");
+        parse("(http|https)://foo.bar/");
     }
 
     @Test
     public void hostnames() {
-        check("http://(first|second)/");
+        parse("http://(first|second)/");
     }
 
     @Test
     public void paths() {
-        check("http://host/(p1|p2)");
+        parse("http://host/(p1|p2)");
     }
 
     @Test
     public void context() {
-        check("http://host/context!");
-        check("http://host/context!path");
-        check("http://host/c1/c2!(p1|p2)");
+        parse("http://host/context!");
+        parse("http://host/context!path");
+        parse("http://host/c1/c2!(p1|p2)");
     }
 
-    private void check(String urlstr) {
-        check(urlstr, urlstr);
+    @Test
+    public void subst() {
+        Map<Character, String> map;
+        Url url;
+
+        map = new HashMap<>();
+        map.put('p', "proto");
+        map.put('h', "host");
+        map.put('s', "segment");
+        url = Url.parse("%p://h%h/con%stext");
+        url = url.sustitute(map);
+        assertEquals("proto://hhost/consegmenttext", url.toString());
     }
 
-    private void check(String urlstr, String normalized) {
+    @Test
+    public void map() {
+        map("http://host/path", "http://host/path");
+        map("(http|https)://h", "http://h/", "https://h/");
+    }
+
+    public void map(String urlstr, String ... mapped) {
+        Url url;
+
+        url = Url.parse(urlstr);
+        assertEquals(Arrays.asList(mapped), url.map());
+    }
+
+    private void parse(String urlstr) {
+        parse(urlstr, urlstr);
+    }
+
+    private void parse(String urlstr, String normalized) {
         Url url;
 
         url = Url.parse(urlstr);
