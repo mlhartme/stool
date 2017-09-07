abcbb0c4ee84951646a976c046d2885949166935
Michael Hartmeier <mlhartme@github.com>
2017-09-07 14:37:04 +0200

    merge tests

--- main/src/test/java/net/oneandone/stool/docker/EngineIT.java
+++ main/src/test/java/net/oneandone/stool/docker/EngineIT.java
@@ -3,6 +3,7 @@ package net.oneandone.stool.docker;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -10,33 +11,30 @@ import static org.junit.Assert.assertTrue;
 
 public class EngineIT {
     @Test
-    public void images() throws IOException {
+    public void turnaround() throws IOException {
+        String message;
         Engine engine;
         String output;
+        String id;
+
+        message = UUID.randomUUID().toString();
+        System.out.println("message " + message);
 
         engine = Engine.open();
-        output = engine.build("mhmtest", "FROM debian:stretch-slim\nCMD [\"ls\", \"-la\", \"/\"]\n");
+        output = engine.build("mhmtest", "FROM debian:stretch-slim\nCMD [\"echo\", \"" + message + "\", \"/\"]\n");
         System.out.println(output);
         assertNotNull(output);
-        engine.imageRemove("mhmtest");
-    }
 
-    @Test
-    public void containers() throws IOException {
-        Engine engine;
-        String id;
-        String output;
-
-        engine = Engine.open();
-        id = engine.containerCreate("hello-world");
+        id = engine.containerCreate("mhmtest");
         assertNotNull(id);
         engine.containerStart(id);
         assertEquals("running", engine.containerStatus(id));
         assertEquals(0, engine.containerWait(id));
         assertEquals("exited", engine.containerStatus(id));
         output = engine.containerLogs(id);
-        assertTrue(output, output.contains("Hello from Docker"));
+        assertTrue(output + " vs" + message, output.contains(message));
         engine.containerRemove(id);
 
+        engine.imageRemove("mhmtest");
     }
 }
