9138767d1eae8c445f8621f68ed5958992888f27
Michael Hartmeier <mlhartme@github.com>
2018-02-06 13:26:24 +0100

    simplify

--- main/src/main/java/net/oneandone/stool/templates/Tomcat.java
+++ main/src/main/java/net/oneandone/stool/templates/Tomcat.java
@@ -15,34 +15,14 @@
  */
 package net.oneandone.stool.templates;
 
-import net.oneandone.inline.Console;
 import net.oneandone.stool.stage.Stage;
-import net.oneandone.stool.util.ServerXml;
 import net.oneandone.sushi.fs.file.FileNode;
-import net.oneandone.sushi.launcher.Launcher;
-import net.oneandone.sushi.util.Separator;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Tomcat implements Template {
-    private static final String APPLOGS = "tomcat/logs/applogs";
-
-    private final String mode;
-
-    private boolean fault;
-    private String faultProject;
-
-
     public Tomcat() {
-        this("test");
-    }
-
-    public Tomcat(String mode) {
-        this.mode = mode;
-        this.fault = false;
-        this.faultProject = "";
     }
 
     @Override
@@ -51,8 +31,7 @@ public class Tomcat implements Template {
     }
 
     @Override
-    public void beforeStart(Stage stage) throws IOException {
-        stage.getBackstage().join(APPLOGS).mkdirOpt();
+    public void beforeStart(Stage stage) {
     }
 
     @Override
@@ -61,11 +40,6 @@ public class Tomcat implements Template {
 
     @Override
     public void contextParameter(Stage stage, String host, int httpPort, FileNode webinf, Map<String, String> result) {
-        String app;
-
-        app = host.substring(0, host.indexOf('.'));
-        result.put("mode", mode);
-        result.put("logroot", ServerXml.toMount(stage.getDirectory(), stage.getBackstage().join(APPLOGS, app).getAbsolute()));
     }
 
     @Override
@@ -74,47 +48,9 @@ public class Tomcat implements Template {
 
     @Override
     public void containerOpts(Stage stage, Map<String, Object> result) {
-        if (fault) {
-            result.put("fault", fault);
-            result.put("fault_project", projects());
-        }
-    }
-
-    // TODO: have a list of projects; always prepend @
-    private String projects() {
-        StringBuilder result;
-
-        if (faultProject.isEmpty()) {
-            return faultProject;
-        }
-        result = new StringBuilder();
-        for (String entry : Separator.SPACE.split(faultProject)) {
-            if (result.length() > 0) {
-                result.append(' ');
-            }
-            if (!entry.startsWith("@")) {
-                result.append('@');
-            }
-            result.append(entry);
-        }
-        return result.toString();
     }
 
     @Override
-    public void files(Stage stage, FileNode dir) throws IOException {
-        Console console;
-        Launcher launcher;
-
-        if (fault) {
-            launcher = stage.launcher("fault");
-            console = stage.session.console;
-            if (console.getVerbose()) {
-                launcher.arg("-v");
-            }
-            launcher.arg("-auth=false");
-            launcher.arg("run", projects(), "cp", "-r", dir.getWorld().getHome().join(".fault").getAbsolute(), dir.getAbsolute());
-            console.verbose.println("executing " + launcher);
-            console.verbose.println(launcher.exec());
-        }
+    public void files(Stage stage, FileNode dir) {
     }
 }
