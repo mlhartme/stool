8ce2014df9bebdd593fcf50d6528682147381188
Michael Hartmeier <mlhartme@github.com>
2019-02-25 08:50:19 +0100

    improve start/restart arguments

--- main/src/main/java/net/oneandone/stool/cli/Main.java
+++ main/src/main/java/net/oneandone/stool/cli/Main.java
@@ -98,8 +98,8 @@ public class Main {
                       cli.add(History.class, "history -details=@history.details:false -max=@history.max:50");
                       cli.add(Ls.class, "list -defaults=@list.defaults info* { select*(info) }");
                       cli.add(Remove.class, "remove -batch -stop");
-                      cli.add(Restart.class, "restart -image");
-                      cli.add(Start.class, "start -tail -image");
+                      cli.add(Restart.class, "restart image?=0");
+                      cli.add(Start.class, "start -tail image?=0");
                       cli.add(Status.class, "status -defaults=@status.defaults info* { select*(info) }");
                       cli.add(Stop.class, "stop");
                       cli.add(Validate.class, "validate -email -repair");
