714611b039033fa50f11520713bd483e1b973684
Michael Hartmeier <mlhartme@github.com>
2019-04-05 11:28:45 +0200

    truely server stage properties

--- server/src/main/java/net/oneandone/stool/server/cli/RestController.java
+++ server/src/main/java/net/oneandone/stool/server/cli/RestController.java
@@ -2,16 +2,33 @@ package net.oneandone.stool.server.cli;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import net.oneandone.stool.common.Reference;
+import net.oneandone.stool.server.util.Property;
 import net.oneandone.stool.server.util.Server;
+import net.oneandone.stool.server.util.Session;
 import net.oneandone.sushi.fs.World;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
 @Controller
 public class RestController {
+    private final Session session;
+
+    public RestController() throws IOException {
+        World world;
+        Globals globals;
+
+        world = World.create();
+        globals = Globals.create(world, world.file("/Users/mhm/Projects/github.com/net/oneandone/stool/stool/client/target/it/git"), // TODO
+                new String[0]);
+        this.session = globals.session();
+        System.out.println("session created: " + session);
+    }
+
     @GetMapping("/version") @ResponseBody
     public String version() throws IOException {
         String version;
@@ -20,13 +37,14 @@ public class RestController {
         return "<html><title>version=" + version + "</title></html>";
     }
 
-    @GetMapping("/stage/${name}/properties") @ResponseBody
-    public String properties() throws IOException {
+    @GetMapping("/stage/{stage}/properties") @ResponseBody
+    public String properties(@PathVariable(value = "stage") String stage) throws IOException {
         JsonObject result;
 
-        System.out.println("properties");
         result = new JsonObject();
-        result.add("quota", new JsonPrimitive("11000"));
+        for (Property property : session.load(new Reference(stage)).properties()) {
+            result.add(property.name(), new JsonPrimitive(property.get()));
+        }
         return result.toString();
     }
 }
