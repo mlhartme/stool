c49c81cc14b494f1a1a40f9bc5744f739692005e
Michael Hartmeier <mlhartme@github.com>
2019-04-05 09:23:10 +0200

    sushi fix

--- server/src/main/java/net/oneandone/stool/server/cli/RestController.java
+++ server/src/main/java/net/oneandone/stool/server/cli/RestController.java
@@ -1,13 +1,20 @@
 package net.oneandone.stool.server.cli;
 
+import net.oneandone.stool.server.util.Server;
+import net.oneandone.sushi.fs.World;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+
 @Controller
 public class RestController {
     @GetMapping("/version") @ResponseBody
-    public String version() {
-        return "<html><title>version</title></html>";
+    public String version() throws IOException {
+        String version;
+
+        version = Server.versionString(World.create());
+        return "<html><title>version=" + version + "</title></html>";
     }
 }
