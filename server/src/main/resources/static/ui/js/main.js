bc22f5e459223d32f965541f5150198beb820307
Michael Hartmeier <mlhartme@github.com>
2019-05-10 10:59:06 +0200

    start and stop ok

--- server/src/main/resources/static/ui/js/main.js
+++ server/src/main/resources/static/ui/js/main.js
@@ -11,10 +11,6 @@ dashboard = {
         init: function () {
             $('#search').on('keyup', dashboard.stages.filter);
             $('#category').on('change', dashboard.stages.filter);
-            $('.modal').on('hide.bs.modal', function () {
-                $(".shell").html("");
-                dashboard.stages.reload();
-            });
         }
     },
 
@@ -78,7 +74,7 @@ dashboard = {
         },
 
         action: function () {
-            var stage, action, arguments, p, i, url;
+            var stage, action, arguments, i, url, box;
             if ($(this).parent().hasClass('disabled') && $(this).attr('data-stage') === null) {
                 return false;
             }
@@ -86,59 +82,24 @@ dashboard = {
             stage = $(this).attr('data-stage');
             action = $(this).attr('data-action');
             arguments = $(this).attr('data-arguments');
-            p = $(this).parent().parent();
 
             url = "/api/stages/" + stage + "/" + action;
             $('#' + stage + ' a.action').addClass('disabled');
-
-            $(p).find('.fa').toggle(true);
-            $(p).find('.badge').attr('class', 'badge badge-primary').html('n/a');
-
             if (arguments != null) {
                 url = url + "?" + arguments;
             }
-            $('#console').modal();
+            box = $('#console');
+            box.modal('show');
+            box.find('.modal-header').html("<p>" + stage + " " + action + "</p>");
             $.post(url).fail(function (r) {
-                $(p).find('.fa').toggle(false);
-                $(p).find('.badge').attr('class', 'badge badge-warning').html('broken: ' + r);
-                $(p).addClass('warning');
+                box.find('.modal-body').html('failed: ' + r);
                 console.log(r);
             }).done(function (r) {
+                box.modal('hide')
                 console.log('success ' + r);
-                var spinner;
-                $('.modal-body .shell').html("").show();
-                spinner = '.fa-spinner';
-                $(spinner).show();
-                dashboard.stages.showLog('.modal-body .shell', r, 0, spinner, '.modal');
-
             });
+            dashboard.stages.reload();
             return false;
-        },
-
-        showLog: function (element, id, index, spinner, parent) {
-            dashboard.stages.fetchLog(element, id, index, spinner, 0, parent);
-        },
-
-        fetchLog: function (element, id, index, spinner, lastSize, parent) {
-            $.get("/processes/" + id + "/log", {"index": index}).done(function (data, status, response) {
-                newSize = response.getResponseHeader("X-Size");
-                if (newSize !== lastSize) {
-                    lastSize = newSize;
-                    if ($(element).text().length > 100000) {
-                        $(element).empty();
-                    }
-                    $(element).append(data);
-                    $(parent).animate({scrollTop: $(element).height()}, 'fast');
-                }
-                if ("true" === response.getResponseHeader("X-Running")) {
-                    setTimeout(function () {
-                        dashboard.stages.fetchLog(element, id, index, spinner, lastSize, parent);
-                    }, 1000);
-                } else {
-                    $(spinner).toggle(false);
-                }
-            });
-
         },
 
         filter: function () {
