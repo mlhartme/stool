60db8921f24332adfb1119a0a3b5c7b5be539f98
Michael Hartmeier <mlhartme.users.noreply@github.com>
2020-03-02 14:38:29 +0100

    merge k8s engine into Docker engine

--- server/src/main/java/net/oneandone/stool/server/docker/Engine.java
+++ server/src/main/java/net/oneandone/stool/server/docker/Engine.java
@@ -15,12 +15,23 @@
  */
 package net.oneandone.stool.server.docker;
 
+import ch.qos.logback.classic.util.ContextInitializer;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSyntaxException;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1DeleteOptions;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodBuilder;
+import io.kubernetes.client.util.Config;
 import jnr.unixsocket.UnixSocketAddress;
 import jnr.unixsocket.UnixSocketChannel;
 import net.oneandone.stool.server.ArgumentException;
@@ -62,6 +73,63 @@ import java.util.Set;
  * Not thread-safe because the io buffer is shared.
  */
 public class Engine implements AutoCloseable {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        FileNode f;
+
+        f = World.createMinimal().guessProjectHome(Engine.class).join("src/test/resources/logback-test.xml");
+        System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, f.getAbsolute());
+
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+        CoreV1Api api;
+        V1Pod pod;
+
+        // client.setDebugging(true);
+        api = new CoreV1Api();
+        pod = pod("pod", "contargo.server.lan/cisoops-public/hellowar:1.0.0");
+        System.out.println("before: " + pod);
+        try {
+            pod = api.createNamespacedPod("stool", pod, null, null, null);
+            System.out.println("after: " + pod);
+            Thread.sleep(2000);
+            try {
+                api.deleteNamespacedPod("pod", "stool", null,
+                        null, null, null, null,
+                        new V1DeleteOptions());
+            } catch (JsonSyntaxException e) {
+                if (e.getMessage().contains("java.lang.IllegalStateException: Expected a string but was BEGIN_OBJECT")) {
+                    // TODO The Java Client is generated, and this code generation does not support union return types,
+                    //      see https://github.com/kubernetes-client/java/issues/86
+                    // TODO: check if pod was actually deletes
+                    // fall-through
+                } else {
+                    throw e;
+                }
+            }
+        } catch (ApiException e) {
+            throw new IOException("apiException: " + e.getMessage() + " " + e.getResponseBody(), e);
+        }
+
+        // TODO: https://github.com/kubernetes-client/java/issues/865
+        client.getHttpClient().connectionPool().evictAll();
+    }
+
+    private static V1Pod pod(String name, String image) {
+        return new V1PodBuilder()
+                .withNewMetadata().withName(name).endMetadata()
+                .withNewSpec()
+                .addNewContainer().withName(name + "-container").withImage(image).endContainer().endSpec().build();
+    }
+
+    private static V1ObjectMeta md(String name) {
+        V1ObjectMeta md;
+
+        md = new V1ObjectMeta();
+        md.setName(name);
+        return md;
+    }
+    //--
+
     public static final DateTimeFormatter CREATED_FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.n'Z'");
 
     public enum Status {
