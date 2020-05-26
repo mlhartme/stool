a2581a921dfa78a220f5a11ee9a2a31e6c3c3ac2
Michael Hartmeier <mlhartme.users.noreply@github.com>
2020-05-26 15:31:12 +0200

    fixes

--- client/src/main/java/net/oneandone/stool/client/cli/Setup.java
+++ client/src/main/java/net/oneandone/stool/client/cli/Setup.java
@@ -38,7 +38,7 @@ import java.util.Map;
 import java.util.Properties;
 
 public class Setup {
-    private static final String LOCALHOST = "localhost";
+    private static final String LOCALHOST = "localhost"; // TODO
 
     private final World world;
     private final Gson gson;
@@ -310,7 +310,7 @@ public class Setup {
         env = new LinkedHashMap<>();
         debugPort = Integer.toString(port + 1);
         addIfNew(env, "HOST", LOCALHOST);
-        addIfNew(env, "OPTS", "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,quiet=n,address=*:" + debugPort);
+        addIfNew(env, "OPTS", "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,quiet=n,address=" + debugPort); // TODO: prefix port with :* when back zo Java 14
         for (Map.Entry<String, String> entry : opts.entrySet()) {
             addIfNew(env, entry.getKey(), entry.getValue());
         }
