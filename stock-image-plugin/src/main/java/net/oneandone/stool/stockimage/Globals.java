0d8c5e21c3f91519972b9fb03c9c653b734dead8
Michael Hartmeier <mlhartme.users.noreply@github.com>
2020-10-26 17:39:08 +0100

    simplify

--- stock-image-plugin/src/main/java/net/oneandone/stool/stockimage/Globals.java
+++ stock-image-plugin/src/main/java/net/oneandone/stool/stockimage/Globals.java
@@ -21,65 +21,18 @@ import net.oneandone.sushi.fs.ExistsException;
 import net.oneandone.sushi.fs.World;
 import net.oneandone.sushi.fs.file.FileNode;
 
-import java.util.UUID;
-
 /** Global client stuff */
 public class Globals {
-    public static Globals create(Console console, World world, FileNode stoolYamlOpt, String command) {
-        FileNode scYaml;
-        String str;
-
-        if (stoolYamlOpt != null) {
-            scYaml = stoolYamlOpt;
-        } else {
-            str = System.getenv("SC_YAML");
-            if (str != null) {
-                scYaml = world.file(str);
-            } else {
-                scYaml = world.getHome().join(".sc.yaml");
-            }
-        }
-        return new Globals(console, world, scYaml, UUID.randomUUID().toString(), command);
-    }
-
     private final Console console;
     private final World world;
-    private final FileNode scYaml;
-    private final String invocation;
-    private final String command;
-    private String context;
-    private FileNode wirelog;
 
-    public Globals(Console console, World world, FileNode scYaml, String invocation, String command) {
+    public Globals(Console console, World world) {
         this.console = console;
         this.world = world;
-        this.scYaml = scYaml;
-        this.invocation = invocation;
-        this.command = command;
-        this.context = null;
-        this.wirelog = null;
-    }
-
-    public FileNode scYaml() {
-        return scYaml;
     }
 
     public FileNode templates() throws ExistsException, DirectoryNotFoundException {
         return world.file(System.getenv("CISOTOOLS_HOME")).join("stool/templates-6").checkDirectory(); // TODO
-    }
-
-    public void setWirelog(String wirelog) {
-        this.wirelog = wirelog == null ? null : world.file(wirelog);
-    }
-
-    public void setException(boolean exception) {
-        if (exception) {
-            throw new RuntimeException("intentional exception");
-        }
-    }
-
-    public void setContext(String context) {
-        this.context = context;
     }
 
     public World getWorld() {
