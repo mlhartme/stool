b0b52dd2753ee556f4a254adf0285f760387193c
Michael Hartmeier <mlhartme.users.noreply@github.com>
2020-12-28 07:44:34 +0100

    use my custom launcher

---
+++ src/launcher.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+APP="$0"
+while [ -h "$APP" ] ; do
+  ls=$(ls -ld "$APP")
+  link=$(expr "$ls" : '.*-> \(.*\)$')
+  if expr "$link" : '/.*' > /dev/null; then
+    APP="$link"
+  else
+    APP=$(dirname "$APP")"/$link"
+  fi
+done
+NAME=$(basename "$APP")
+APP=$(dirname "$APP")
+APP=$(cd "$APP" && pwd)
+APP="$APP/$NAME"
+
+java -Djava.awt.headless=true --illegal-access=warn $SC_OPTS -jar "$APP" "$@"
+exit $?
