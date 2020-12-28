c4376a0c3fed486c6b1fb26d001b023435aeb1a4
Michael Hartmeier <mlhartme.users.noreply@github.com>
2020-12-28 08:07:16 +0100

    dump lazy cert checks, puki has a proper chain now (and it's unused anyway)

--- src/main/java/net/oneandone/stool/server/Main.java
+++ src/main/java/net/oneandone/stool/server/Main.java
@@ -43,23 +43,13 @@ import net.oneandone.stool.cli.command.Version;
 import net.oneandone.sushi.fs.World;
 import net.oneandone.sushi.fs.file.FileNode;
 import net.oneandone.sushi.fs.http.HttpFilesystem;
-import net.oneandone.sushi.fs.http.Proxy;
 import net.oneandone.sushi.io.PrefixWriter;
 import net.oneandone.sushi.util.Separator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
@@ -135,72 +125,13 @@ public class Main {
         return cli.run(args);
     }
 
-    //--
-
-    private static final List<String> LAZY_HOSTS = Arrays.asList("api-next.pki.1and1.org");
-
     public static World world() throws IOException {
         World world;
 
         world = World.create();
-        update(world, "http");
-        update(world, "https");
         if (System.getProperty("stool.wire") != null) {
             HttpFilesystem.wireLog("/tmp/stool.wire");
         }
         return world;
-    }
-
-    private static void update(World world, String scheme) {
-        HttpFilesystem fs;
-        Proxy proxy;
-
-        fs = (HttpFilesystem) world.getFilesystem(scheme);
-        fs.setSocketFactorySelector((protocol, hostname) ->
-                protocol.equals("https") ? (LAZY_HOSTS.contains(hostname) ? lazyFactory() : SSLSocketFactory.getDefault()) : null);
-        if (fs.getProxy(scheme) == null) {
-            proxy = Proxy.forPropertiesOpt("stool." + scheme);
-            if (proxy == null) {
-                proxy = Proxy.forPropertiesOpt(scheme);
-                if (proxy == null) {
-                    proxy = Proxy.forEnvOpt(scheme);
-                }
-            }
-            if (proxy != null) {
-                fs.setProxy(scheme, proxy);
-            }
-        }
-    }
-
-    public static SSLSocketFactory lazyFactory() {
-        TrustManager[] trustAllCerts;
-        SSLContext sc;
-
-        trustAllCerts = new TrustManager[] {
-                new X509TrustManager() {
-                    @Override
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
-                    @Override
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                    }
-
-                    @Override
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
-        try {
-            sc = SSLContext.getInstance("SSL");
-        } catch (NoSuchAlgorithmException e) {
-            throw new IllegalArgumentException(e);
-        }
-        try {
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-        } catch (KeyManagementException e) {
-            throw new IllegalArgumentException(e);
-        }
-        return sc.getSocketFactory();
     }
 }
